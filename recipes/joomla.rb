d3c7537b05446009781815ea897f16c83f3f12c6
David Pando <david.pando@gmail.com>
2015-02-02 20:59:33 +1300

    joomla recipe

---
+++ recipes/joomla.rb
@@ -0,0 +1,23 @@
+joomla_package = node['hwdmediashare']['joomla_package_url'].gsub(/^(.*\/)/,'')
+remote_file "#{Chef::Config[:file_cache_path]}/#{joomla_package}" do
+    source node['hwdmediashare']['joomla_package_url']
+end
+
+execute 'unpack joomla' do
+  user node['hwdmediashare']['user']
+  command "tar xzf #{Chef::Config[:file_cache_path]}/#{joomla_package} -C #{node['hwdmediashare']['docroot']}"
+  creates "#{node['hwdmediashare']['docroot']}/index.php"
+end
+
+directory "#{node['hwdmediashare']['docroot']}/cgi-bin" do
+  owner node['hwdmediashare']['user']
+  group node['hwdmediashare']['group']
+  mode 0755
+end
+
+cookbook_file "#{node['hwdmediashare']['docroot']}/cgi-bin/ubr_upload.pl" do
+  source 'ubr_upload.pl'
+  owner node['hwdmediashare']['user']
+  group node['hwdmediashare']['group']
+  mode 0755
+end
